250ef3df046eec21a4978a8cecc3988305af8f74
Andrew Sutherland <asutherland@asutherland.org>
2016-04-10 00:07:55 -0400

    computer bounce

--- src/sims/cache/api.js
+++ src/sims/cache/api.js
@@ -31,7 +31,6 @@ Cache.prototype = {
     /// 1. Let r be null.
     // => temporary to hold a normalized version of "request" so that it's a
     // proper Request and has no fragment.
-
     /// 2. If request is a Request object, then:
     /// 2.1. Set r to request’s request.
     /// 2.2. If r’s url’s scheme is not one of "http" and "https", or r’s method
@@ -44,7 +43,30 @@ Cache.prototype = {
     /// promise rejected with a TypeError.
     // => The actual normalization, plus enforce that Caches only store GETs.
     /// 4. Set r’s url’s fragment to null.
-    // =>
+    // => The spec says fetch URLs discard the fragment, so there's no point
+    // keeping this around.  However, the spec is possibly changing on this
+    // point.
+    /// 5. If response’s associated response’s header list contains a header
+    /// named `Vary`, then:
+    /// 5.1. Let varyHeaders be the array containing the elements corresponding
+    /// to the field-values of the Vary header.
+    /// 5.2.  For each f in varyHeaders:
+    /// 5.2.1. If f matches "*", return a promise rejected with a TypeError.
+    // => A vary response of "*" means that the server is caching based on
+    // things so complex and unknowable that the client can't know.  This is
+    // rarely used since it means things are uncacheable and in those cases it
+    // makes more sense to just indicate that using the more appropriate cache
+    // headers.  But we do throw because it makes vary matching impossible.
+    /// 6. If response is disturbed or locked, return a promise rejected with a
+    /// TypeError.
+    // => Check that the response hasn't been read from/canceled (disturbed) or
+    // locked for reading by a reader (locked).  This ensures that the entirety
+    // of the stream is available for reading so we can write it to disk.
+    /// 7. Let newResponse be a new Response object associated with response’s
+    /// associated response and a new Headers object whose guard is response’s
+    /// Headers' guard.
+    // => 
+
 
     this._emit({
       queue: 'cache',
